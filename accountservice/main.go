69722ff396d1562fc937929c762360504db5c97f
Zhang Mingxiang <zack_zhang@invastsec.jp>
2018-08-07 18:56:14 +0900

    update package name

--- accountservice/main.go
+++ accountservice/main.go
@@ -2,7 +2,7 @@ package main
 
 import (
         "fmt"
-        "github.com/callistaenterprise/goblog/accountservice/service"
+        "github.com/hitxiang/goblog/accountservice/service"
 )
 
 var appName = "accountservice"
